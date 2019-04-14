9de1b48d628866a283ce3f10e1628c621d8180d6
Sergey Sokolov <sSokolov@ipm.local>
2019-04-14 07:50:55 +0300

    hometask 3

--- lesson_1/conftest.py
+++ lesson_1/conftest.py
@@ -1,27 +1,35 @@
+"""Module with fixtures for tests"""
+
 import pytest
 
 
 @pytest.fixture(scope='module', autouse=True)
 def module_data():
+    """Fixture for module"""
     print("\n    > Module setup")
+    print(module_data.__doc__)
 
     yield
 
     print("\n   > Module teardown")
 
 
-@pytest.fixture(scope='class')
+@pytest.fixture(scope='class', autouse=True)
 def suite_data():
+    """Fixture for testsuite"""
     print("\n   > Suite setup")
+    print(suite_data.__doc__)
 
     yield
 
     print("\n   > Suite teardown")
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope='function', autouse=True)
 def case_data():
+    """Fixture for testcase"""
     print("\n    > Case setup")
+    print(case_data.__doc__)
 
     yield
 
--- lesson_2/test_lesson_1_1.py
+++ lesson_2/test_lesson_1_1.py
@@ -4,57 +4,57 @@
 class TestSuite:
     """Testsuite of tests for base types"""
 
-    def test_multiply_numbers(self):
+    def test_multiply_numbers(self, case_data):
         """Number multipluing test"""
         assert 3*4 == 12
 
-    def test_sum_numbers(self):
+    def test_sum_numbers(self, case_data):
         """Number sum test"""
         assert (3 + 4) == 7
 
-    def test_subtr_numbers(self):
+    def test_subtr_numbers(self, case_data):
         """Number subtraction test"""
         assert (4 - 3) == 1
 
-    def test_strings_a_3(self):
+    def test_strings_a_3(self, case_data):
         """String multipluing test"""
         assert "a"*3 == 'aaa'
 
-    def test_string_concatenation(self):
+    def test_string_concatenation(self, case_data):
         """String concatenation test"""
         assert ("aa" + "bb") == "aabb"
 
-    def test_list_len(self):
+    def test_list_len(self, case_data):
         """List length test"""
         rang_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
         assert len(rang_list) == 10
 
-    def test_list_min(self):
+    def test_list_min(self, case_data):
         """Min element test"""
         numbers = [10, 20, 30, 40]
         assert min(numbers) == 10
 
-    def test_list_max(self):
+    def test_list_max(self, case_data):
         """Max element test"""
         numbers_2 = [10, 20, 30, 40]
         assert max(numbers_2) == 40
 
-    def test_list_sum(self):
+    def test_list_sum(self, case_data):
         """List elements test"""
         numbers_3 = [10, 20, 30, 40]
         assert sum(numbers_3) == 100
 
-    def test_dict_key(self):
+    def test_dict_key(self, case_data):
         """Dict element access test"""
         beatles_map = {'Paul': 'Bass', 'John': 'Guitar', 'George': 'Guitar'}
         assert beatles_map['Paul'] == 'Bass'
 
-    def test_dict_pop(self):
+    def test_dict_pop(self, case_data):
         """Dict element pop test"""
         beatles_map_2 = {'Paul': 'Bass', 'John': 'Guitar', 'George': 'Guitar'}
         assert beatles_map_2.pop('Paul') == 'Bass'
 
-    def test_set_inter(self):
+    def test_set_inter(self, case_data):
         """Intersection sets test"""
         odd_set = {1, 3, 5, 7, 9}
         even_set = {0, 2, 4, 6, 8}
