ea04b179e557236371d9c866d21d0b335a9ccdc2
Sergey Sokolov <sSokolov@ipm.local>
2019-08-03 15:57:21 +0300

    lesson_27

---
+++ lesson_27/test_lesson_27.py
@@ -0,0 +1,11 @@
+"""Module with testsuite of tests for JSON API tests"""
+from LoginPage import LoginPage
+
+
+class TestSuite:
+    """Testsuite for base page tests"""
+    def test_is_base_page(self, get_driver, cmdopt_url):
+        """test for is base page"""
+        login_page = LoginPage(get_driver, cmdopt_url)
+        login_page.navigate()
+        assert "Авторизация" in login_page.get_title()
